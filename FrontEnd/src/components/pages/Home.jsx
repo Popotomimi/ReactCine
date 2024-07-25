344f833d4b2bdeb25c6a522ae71cd7ded9a2ade5
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2024-07-24 22:22:29 -0300

    Third Commit
    Third Commit

--- FrontEnd/src/components/pages/Home.jsx
+++ FrontEnd/src/components/pages/Home.jsx
@@ -21,18 +21,18 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="background-image">
+    <div>
       <div className="home">
-        <h1>Filmes em cartaz</h1>
-        <ul>
+        <h1>Filmes em cartaz:</h1>
+        <ul className="movie-container">
           {movies.map((movie) => (
-            <div key={movie.id}>
+            <div className="movie-card" key={movie.id}>
               <img
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 alt={movie.title}
               />
               <h3>Título: {movie.title}</h3>
-              <p>Data de lançamento: {movie.release_data}</p>
+              <button>Mais Detalhes</button>
             </div>
           ))}
         </ul>
--- FrontEnd/src/components/pages/Login.jsx
+++ FrontEnd/src/components/pages/Login.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    window.alert("Robertinho Dev!");
+  };
+
+  return (
+    <div className="background-image">
+      <div className="login">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>E-mail</label>
+            <input type="email" placeholder="Digite seu e-mail" />
+          </div>
+          <div>
+            <label>Senha</label>
+            <input type="password" placeholder="Digite sua senha" />
+          </div>
+          <p>
+            Ainda não tem conta? <Link to="/register">Clique aqui</Link>{" "}
+          </p>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
